ab5da9260dcd02e436c39fccd48859641a354b55
bardia01 <bardia.mzad@gmail.com>
2022-03-01 20:14:57 +0000

    tried to add a way to separate the data into x,y,z data
    Unrelated to this:
    
    The data has a lag when it is sent to the python or nios terminal which is super annoying and I don't know how to get rid of it. Also we still can't show negative values unfortunately

--- test/testing.py
+++ test/testing.py
@@ -10,10 +10,59 @@ def hex_to_dec(h):
 
 
 def readJtag():
-
+    counter = 0
     output = subprocess.Popen('B:\\Quartus\\quartus\\bin64\\nios2-terminal.exe', shell=True, stdout=subprocess.PIPE)
-    
+    x_data = 0
+    y_data = 0
+    z_data = 0
     while(True):
+        initial_line = output.stdout.readline()
+        if len(initial_line) < 14:
+            """
+            line = output.stdout.readline()
+            line = line.decode("utf-8")
+
+            line1 = output.stdout.readline()
+            line1 = line1.decode("utf-8")
+            
+            line2 = output.stdout.readline()
+            line2 = line2.decode("utf-8")
+            """
+            if counter>3:
+                counter = 1
+            counter = counter + 1
+
+            line = output.stdout.readline()
+            line = line.decode("utf-8")
+            
+
+            if line[0] == "x":
+                x_data = line[2:]
+            elif line[0] == "z":
+                z_data = line[2:]
+            elif line[0] == "y":
+                y_data = line[2:]
+            
+            if counter==3:
+                print("x=",x_data,"y=",y_data, "z=", z_data)
+                time.sleep(0.5)
+            #line = output.stdout.readline()
+            #line = line.decode("utf-8")
+            #y_data = line
+
+            #line = output.stdout.readline()
+            #line = line.decode("utf-8")
+            #z_data = line
+            
+            #if(x_data):
+                #print("this is the line ", x_data)
+                #print("this is the int", int(x_data, 16), " in our func", hex_to_dec(x_data))
+            #print(hex_to_dec(line))
+            #print("this is the type ",type(line))
+            #print("this is the length ",len(line))
+            #time.sleep(0.5)
+
+
         #initial_line = str(output.stdout.readline())
         #if len(initial_line) > 14 or len(initial_line) < 8:
             #continue
@@ -21,15 +70,12 @@ def readJtag():
             #line = initial_line[2:][:-5]
             #print(hex_to_dec(line))
             #print(type(line))
-            line = output.stdout.readline()
-            line = line.decode("utf-8")
+            
             #print("this is the initial line", initial_line)
-            print("this is the line ", line)
-            print("this is the type ",type(line))
-            print("this is the length ",len(line))
+            
             # print("this is the int", int(line, 16))
             #print("this is the int ",int(line, 16))
-            time.sleep(0.5)
+        
         
 def main():
     readJtag()
