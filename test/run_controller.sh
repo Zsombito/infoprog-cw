b1dcd0991fad3172835988150af769bbdde4f655
rkhoury18 <84289937+rkhoury18@users.noreply.github.com>
2022-03-17 17:19:36 +0000

    button implemented

---
+++ test/run_controller.sh
@@ -0,0 +1,4 @@
+python -m venv C:\Users\User\Documents\GitHub\infoprog-cw\test\venv
+& C:\Users\User\Documents\GitHub\infoprog-cw\test\venv\Scripts\Activate.ps1
+pip install pyautogui
+python testing.py
